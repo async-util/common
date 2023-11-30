90db0d0d62aeadbe3d6aaf37d0d675b63fec361f
talrasha007 <talrasha007@gmail.om>
2023-11-30 18:33:27 +0800

    read data from event.

---
+++ examples/event.mjs
@@ -0,0 +1,32 @@
+import { getDataFromEvent } from '@async-util/common'
+import { EventEmitter } from 'events'
+
+async function main() {
+  const emitter = new EventEmitter()
+  const evid = setInterval(() => emitter.emit('data', Date.now()), 500)
+  setTimeout(() => {
+    clearInterval(evid)
+    emitter.emit('end')
+  }, 5000)
+
+  for await (const data of getDataFromEvent(emitter)) {
+    console.log(data)
+  }
+
+  console.log('event1 done')
+
+  const evid2 = setInterval(() => emitter.emit('data', Date.now()), 500)
+  setTimeout(() => {
+    clearInterval(evid2)
+    emitter.emit('error', 'oops')
+  }, 5000)
+
+  for await (const data of getDataFromEvent(emitter)) {
+    console.log(data)
+  }
+
+  console.log('event2 done')
+}
+
+main().catch(console.error)
+
--- index.mjs
+++ index.mjs
@@ -15,6 +15,67 @@ export function sleep(ms) {
   return new Promise(r => setTimeout(r, ms));
 }
 
+export async function *getDataFromEvent(emitter, dataEvent='data', endEvent='end', errEvent='error') {
+  const queue = []
+  let resolve, reject, err, end = false
+
+  const listener = (data) => {
+    if (resolve) {
+      resolve(data)
+      resolve = null
+    } else {
+      queue.push(data)
+    }
+  }
+
+  const endListener = () => {
+    end = true
+    if (resolve) {
+      resolve()
+      resolve = null
+    }
+  }
+
+  const errorListener = e => {
+    err = e
+    if (reject) {
+      reject(e)
+      reject = null
+    } 
+  }
+
+  const on = emitter.on ? 'on' : 'addEventListener'
+  const off = emitter.off ? 'off' : 'removeEventListener'
+
+  emitter[on](dataEvent, listener)
+  emitter[on](endEvent, endListener)
+  emitter[on](errEvent, errorListener)
+
+  try {
+    while (true) {
+      if (queue.length > 0) {
+        yield queue.shift()
+      } else if (end) {
+        break
+      } else if (err) {
+        throw err
+      } else {
+        const data = await new Promise((r, j) => {
+          resolve = r
+          reject = j
+        })
+
+        if (data !== undefined) yield data
+        else break
+      }
+    }
+  } finally {
+    emitter[off](dataEvent, listener)
+    emitter[off](endEvent, endListener)
+    emitter[off](errEvent, errorListener)
+  }
+}
+
 export function *group(iter, n) {
   let group = [];
   for (const item of iter) {
